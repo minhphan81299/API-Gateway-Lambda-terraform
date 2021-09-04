5025926044c0fe5fadf269b5a271fb9ebede1c9f
minh.phan81299@gmail.com <admin@example.com>
2021-09-04 16:58:41 +0700

    first commit

--- dev/app-infra/provider.tf
+++ dev/app-infra/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-southeast-1"
-  access_key = "AKIAYNI5SHZ7S4U3R4JV"
+  access_key =var.access_key
 
-  secret_key = "v2nkCR9/wgGCFFE3ZkM1NrVUC5NsG89tLP+qaeXm"
+  secret_key = var.secret_key
 }
 
